bb4f3580396967e5aa66cb5071bbe05c6cdc4a6d
BigfootN <bigfoot_arch@protonmail.com>
2017-10-16 19:56:50 +0200

    database and login_file

--- database.php
+++ database.php
@@ -1,112 +1,112 @@
 <?php
 
-final class Database {
+include 'login_file.php';
+
+final class DatabaseN {
 	private $dbh;
+	private $loginFile;
+
+	private static $user;
+	private static $dbname;
+	private static $host;
+	private static $password;
+
 	private static $instance = null;
 
-	public Database() {
-		private function __construct($servername, $user, $pwd, $dbname) {
-			$dsn = "mysql:dbname=" . $dbname . ";host=" . $servername;
+	private static function isSameEntry($login_file) {
+		if (self::$instance === null)
+			return false;
 
-			try {
-				$dbh = new PDO($dsn, $user, $pwd);
-			} catch (PDOException e) {
-				echo "Connection failed: " . $e->getMessage();
-			}
+		if (strcmp(self::$user, $login_file->getUser()) !== 0)
+			return false;
+
+		if (strcmp(self::$dbname, $login_file->getDatabase()) !== 0)
+			return false;
+
+		if (strcmp(self::$host, $login_file->getHost()) !== 0)
+			return false;
+
+		if (strcmp(self::$password, $login_file->getPassword()) !== 0)
+			return false;
+
+		return true;
+	}
+
+	private static function setLoginInfo($login_file) {
+		self::$user = $login_file->getUser();
+		self::$password = $login_file->getPassword();
+		self::$host = $login_file->getHost();
+		self::$dbname = $login_file->getDatabase();
+	}
+
+	private function initConn() {
+		$dsn = "mysql:dbname=" . self::$dbname . ";host=" . self::$host;
+
+		try {
+			echo "dsn = " . self::$password . "\n";
+			$this->dbh = new PDO($dsn, self::$user, self::$password);
+		} catch (PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+		}
+	}
+
+	private function __construct() {}
+
+	public static function Instance($login_file_path) {
+
+		$file = fopen($login_file_path, "r");
+		$login_file = new LoginFile($login_file_path);
+		if (self::$instance === null || !(self::$instance->isSameEntry($login_file))) {
+			self::$instance = new DatabaseN();
+			self::$instance->setLoginInfo($login_file);
+			self::$instance->initConn();
 		}
 
-		public static function Instance($servername, $user, $pwd, $dbname) {
-			if ($instance === null)
-				$instance = new Database($servername, $user, $pwd, $dbname);
+		return self::$instance;
+	}
+
+	public function query($sql, $class_name = null, $one = false) {
+		$req = self::$instance->dbh->query($sql);
+		if($class_name === null) {
+			$req->setFetchMode(PDO::FETCH_OBJ);
+		} else {
+			$req->setFetchMode(PDO::FETCH_CLASS, $class_name);
+		} if($one) {
+			$datas = $req->fetch();
+		} else {
+			$datas = $req->fetchAll();
+		}
+		return $datas;
+	}
+
+	public function prepare($sql, $attributes, $class_name, $one = false) {
+		$req = self::$instance->dbh->prepare($sql);
+		$res = $req->execute($attributes);
+		if(
+			strpos($sql, 'UPDATE') === 0 ||
+			strpos($sql, 'INSERT') === 0 ||
+			strpos($sql, 'DELETE') === 0
+		){
+			return $res;
 		}
 
-		function __destruct() {
-			$conn->close();
+		$req->setFetchMode(PDO::FETCH_CLASS, $class_name);
+		if ($one) {
+			$datas = $req->fetch();
+		} else {
+			$datas = $req->fetchAll();
 		}
 
-		public function getClientId($client_name, $client_first_name) {
-			$sql = "SELECT client_id FROM client WHERE name = :name AND first_name = :first_name";
-			$ret = 0;
+		return $datas;
+	}
 
-			$dbh->beginTransaction();
-			$sth = $dbh->prepare($sql);
+	public function exec($sql) {
+		$req = $this->getPDO()->exec($sql);
+		return $req;
+	}
 
-			$sth->bindParam(":name", $client_name);
-			$sth->bindParam(":first_name", $client_first_name);
-
-			$sth->execute();
-			$result = $sth->fetchAll();
-
-			if ($result->num_rows > 0) {
-				$row = $result->fetch_assoc();
-				$ret = $row["client_id"];
-			}
-
-			return $ret;
-		}
-
-		public function addClient($gender, $name, $first_name, $age, $email) {
-			$gender = (strcmp($gender, "Madame") === 0) ? 1 : 0;
-			$sql = "INSERT INTO client (gender, name, first_name, age, email) VALUES (?, ?, ?, ?, ?)";
-
-			$dbh->beginTransaction();
-			$sth = $dbh->prepare($sql);
-
-			$sth->execute(array(
-				$gender,
-				$name,
-				$first_name,
-				$name,
-				$age,
-				$email
-			));
-
-			$dbh->commit();
-		}
-
-		public function addBuyer($client_name, $client_first_name) {
-			$id = get_client_id($client_name, $client_first_name);
-			$sql = 'INSERT INTO buyer (client_id_fk) VALUES (?)';
-
-			$dbh->beginTransaction();
-			$sth = $dbh->prepare($sql);
-
-			$sth->execute(array($id));
-
-			$dbh->commit();
-		}
-
-		public function addInfoOrder($order_day, $delivery_time, $cash, $buyer_id) {
-			$sql = "INSERT INTO info_order (order_day, delivery_time, cash, buyer_id_fk) VALUES (?, ?, ?, ?)";
-
-			$dbh->beginTransaction();
-			$sth = $dbh->prepare($sql);
-
-			$sth->execute(array(
-				$order_day,
-				$delivery_time,
-				$cash,
-				$buyer_id_fk
-			));
-
-			$dbh->commit();
-		}
-
-		public function addOrder($meal, $rate, $info_order_id, $client_id) {
-			$sql = "INSERT INTO `order` (meal, rate, info_order_id_fk, client_id_fk) VALUES (?, ?, ?, ?)";
-
-			$dbh->beginTransaction();
-			$sth = $dbh->prepare($sql);
-
-			$sth->execute(array(
-				$meal,
-				$rate,
-				$info_order_id,
-				$client_id
-			));
-
-			$dbh->commit();
-		}
+	function __destruct() {
+		$conn->close();
 	}
 }
 
